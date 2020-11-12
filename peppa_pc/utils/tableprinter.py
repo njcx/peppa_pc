9ef60e658097c55559cca36099489dcd6920e161
njcx <njcx86@gmail.com>
2020-11-12 13:48:08 +0800

    add .py

--- peppa_pc/utils/tableprinter.py
+++ peppa_pc/utils/tableprinter.py
@@ -4,10 +4,10 @@ import prettytable as pt
 def data_table_printer(data):
     tb = pt.PrettyTable()
     tb.field_names = ["id", "target", "poc_name", "app_name", "app_version", "vul_type", "info", "mode", "status"]
-    id = 0
+    id_ = 0
     for data_ in data:
-        id += 1
-        tb.add_row([id, data_["target"], data_["poc_name"], data_["app_name"], data_["app_version"],
-                    data_[ "vul_type"], data_[ "info"],data_["mode"], data_["status"]])
+        id_ += 1
+        tb.add_row([id_, data_["target"], data_["poc_name"], data_["app_name"], data_["app_version"],
+                    data_["vul_type"], data_["info"], data_["mode"], data_["status"]])
     return tb
 
